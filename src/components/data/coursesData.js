076bf41888fa5ff2f26a6fd18ea042ed2e441a7f
Vinit <vinitsk99@gmail.com>
2024-08-31 21:59:22 +0530

    Courses data

---
+++ src/components/data/coursesData.js
@@ -0,0 +1,26 @@
+import DBMSCourse from "../../assets/courseImg/DBMSCourse.png";
+import PhpCourse from "../../assets/courseImg/phpCourse.png";
+import JavaCourse from "../../assets/courseImg/javaCourse.jpg";
+
+const coursesData = [
+  {
+    image: DBMSCourse,
+    name: "Database Management System",
+    desc: "Understand the concepts of database management systems, SQL, data modeling, and normalization.",
+    link: "https://dbms7107.netlify.app/"
+  },
+  {
+    image: PhpCourse,
+    name: "PHP",
+    desc: "Explore server-side scripting with PHP, covering topics like web development, database integration, and security.",
+    link: "https://vinit71.github.io/PHP-Website/"
+  },
+  {
+    image: JavaCourse,
+    name: "Core Java",
+    desc: "Learn the fundamentals of Java programming, including object-oriented principles, syntax, and core libraries.",
+    link: "https://java7107.netlify.app/"
+  }
+];
+
+export default coursesData;
